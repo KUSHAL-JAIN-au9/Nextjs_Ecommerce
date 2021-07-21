714b309359775f6bf1547dbcb99416d836a1bdf1
Kushal-Jain-au8 <jkushal069@gmail.com>
2021-07-21 18:49:30 +0530

    config files hidden for security purpose

--- Next js E commerce/config.js
+++ Next js E commerce/config.js
@@ -1,17 +1,6 @@
 const keys= {
 
-    API_KEY :"pk.8ebd3fa463e76e4a36a9153d39412249",
-    BASE_URL:`http://localhost:8000/services`,
-    BASE_PROD_URL:`http://localhost:8000/products`,
-    
-    CORSRemoval_URL:`https://corsanywhere.herokuapp.com/`,
-    BASELOC_URL : `https://eu1.locationiq.com/v1/reverse.php?`,
-    BASE_PRODPOST_URL:`http://localhost:3001/api/createProducts`,
-    CLOUD_UPDATE_PRESET: "gvcrdnvm",
-    CLOUD_NAME: "dkdly1eqg",
-    CLOUD_API: "https://api.cloudinary.com/v1_1/dkdly1eqg/image/upload",
-    CLOUD_API_KEY :"918578469881149",
-    CLOUD_SECRET_KEY:"E24YX02-5twgq-RO4vPTugvl8hs"
+   // all config files here
 
 
 
